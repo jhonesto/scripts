86f73ffab5275e34d1a7e99caacb9977631be95b
jhonesto <jhonesto@protonmail.com>
2021-11-30 00:22:59 -0300

    script para inicializar o springtool suite

---
+++ springtool/springtool.sh
@@ -0,0 +1,14 @@
+X
+#! /bin/sh
+
+cd ~/springtools
+
+export GTK_IM_MODULE="ibus"
+
+export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+
+export PATH=$PATH:$JAVA_HOME/bin
+
+export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
+
+./SpringToolSuite4 &
